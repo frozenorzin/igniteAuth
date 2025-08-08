0005bec590a405ba1a63900b635e640b92560824
Leeladitya Simma <151020662+frozenorzin@users.noreply.github.com>
2025-08-08 22:10:05 +0530

    Update server.js

--- IgniteAuth-V0.4/server.js
+++ IgniteAuth-V0.4/server.js
@@ -1,1 +1,62 @@
+const express = require('express');
 
+const cors = require('cors');
+
+const path = require('path');
+
+const app = express();
+
+const PORT = 3001;
+
+
+// middleware 
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
+
+
+
+
+// static serving 
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+// error code 
+/*
+app.get('/',(req,res)=>{
+
+    res.send('IgniteAuth Test Engines Live');
+
+    
+});
+
+
+
+app.use((req,res,next)=>{
+
+        res.status(404).sendFile(path.join(__dirname,'public','404.html'));
+
+
+    
+});*/
+
+// routing 
+
+const router = require('./routes/routes.js');
+
+app.use('/',router);
+
+
+
+
+
+
+app.listen(PORT, ()=>{
+
+
+    console.log(`listening at ${PORT}`);
+});

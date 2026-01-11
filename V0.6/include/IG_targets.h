fcbc873168741ba7dbb2cf9d18695ac875776308
Leeladitya Simma <151020662+frozenorzin@users.noreply.github.com>
2026-01-11 21:57:49 +0530

    Fix header guard and update executable paths

--- V0.6/include/IG_targets.h
+++ V0.6/include/IG_targets.h
@@ -1,7 +1,10 @@
-#ifndef IG_TARGETS
+//./include/IG_targets.h
+#ifndef IG_TARGETS_H
+#define IG_TARGETS_H
 
-#define SUB_DRIVER = "../bin/sub_driver.exe" 
-#define SUB_GPIO   = "../bin/sub_gpio.exe"
-#define SUB_UART   = "../bin/sub_uart.exe" 
+#define SUB_DRIVER  "./bin/sub_driver.exe" 
+#define SUB_GPIO    "./bin/sub_gpio.exe"
+#define SUB_UART    "./bin/sub_uart.exe" 
+#define SUB_VMWARE  "C:\\Program Files (x86)\\VMware\\VMware Player\\vmplayer.exe"
 
 #endif

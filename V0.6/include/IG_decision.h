a67865f68b077cfca5f047f92b23d0e96661b710
Leeladitya Simma <151020662+frozenorzin@users.noreply.github.com>
2026-01-11 21:57:21 +0530

    Add IG_decision structure definition

--- V0.6/include/IG_decision.h
+++ V0.6/include/IG_decision.h
@@ -1,1 +1,22 @@
-// ig decision 
+// ./include/ig_decision.h
+
+#ifndef IG_DECISION_H
+#define IG_DECISION_H
+
+#include<stdbool.h>
+
+#define IG_MAX_STR 64
+
+typedef struct {
+
+    char sid[IG_MAX_STR];
+    char intent[IG_MAX_STR];
+    char command[IG_MAX_STR];
+    char target[IG_MAX_STR];
+    bool allow;
+    char reason[128];   // empty if allowed
+} IG_decision;
+
+
+
+#endif 
